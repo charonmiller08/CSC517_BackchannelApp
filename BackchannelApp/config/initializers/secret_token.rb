151dfddcd4a7cf844c0343a824f19827266e4b81
Caroline Grimes <grimes.caroline@gmail.com>
2013-09-24 19:02:01 -0400

    Project
    skeleton

---
+++ BackchannelApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackchannelApp::Application.config.secret_token = 'd3123fdec3000a1dfb2652d0283acda2a81d90d91502ddff5e90f239ece13438f5d319b71a84dceb5949b6f2457970a87304d4a3281580a48d75d950255e1af7'
