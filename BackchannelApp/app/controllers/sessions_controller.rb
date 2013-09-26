aaf8ef91040ea5f3f7a21b06576c89914b8146eb
Caroline Grimes <grimes.caroline@gmail.com>
2013-09-25 21:36:25 -0400

    Session Works

--- BackchannelApp/app/controllers/sessions_controller.rb
+++ BackchannelApp/app/controllers/sessions_controller.rb
@@ -3,12 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find(params[:session][:username].downcase)
-
-    if user = User.authenticate(username: params[:session][:username].downcase, params[:session][:password])
+   user = User.find_by_username(params[:session][:username]);
+   if user && User.authenticate(params[:session][:username].downcase, params[:session][:password])
       # Sign the user in and redirect to the user's show page.
-
-    else
+     #puts user.username
+     flash[:notice] = 'Welcome to Backchannel App!'
+     redirect_to root_url
+   else
       # Create an error message and re-render the signin form.
       flash[:error] = 'Invalid email/password combination' # Not quite right!
       render 'new'
--- BackchannelApp/app/models/user.rb
+++ BackchannelApp/app/models/user.rb
@@ -4,13 +4,19 @@ class User < ActiveRecord::Base
   has_many :votes
   has_one :user_role
 
+  before_save { |user| user.username = username.downcase }
+
   validates :password, :presence => true
   validates :name, :presence => true
   validates :username, :presence => true
   validates :username, :uniqueness => true
 
-  def self.authenticate(username, password)
-    @user = User.find(["username = ? AND password = ?", username, password])
-    return @user
+  def self.authenticate(username, submitted_password)
+    user = User.find_by_username(username);
+    if(user && (user.password == submitted_password))
+        return true
+    end
+    return nil if user.nil?
+
   end
 end
