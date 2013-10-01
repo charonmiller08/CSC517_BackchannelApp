a497d2a78f2f80270446fcad4f37e4d53b14e30c
Daniel Kernicky <daniel.kernicky@gmail.com>
2013-10-01 12:18:00 -0400

    More Integration Tests

--- BackchannelApp/test/integration/sign_up_test.rb
+++ BackchannelApp/test/integration/sign_up_test.rb
@@ -4,12 +4,10 @@ class SignUpTest < ActionDispatch::IntegrationTest
   test "sign up with valid username and password" do
     visit login_url
     visit signup_url
-    puts "******"
-    puts signup_url
-    puts "******"
-    fill_in 'username', :with => "user1"
-    fill_in :password, :with => "password"
-    fill_in :password_confirmation, :with => "password"
+    fill_in :user_username, :with => "user1"
+    fill_in :user_password, :with => "password"
+    fill_in :user_password_confirmation, :with => "password"
+    click_button "Signup"
 
   end
   test "unable to sign up if username exists"  do
