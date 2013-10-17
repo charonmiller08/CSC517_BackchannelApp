d8e23297614cd0e5929523efba5a440e5f529fec
Daniel Kernicky <daniel.kernicky@gmail.com>
2013-10-17 12:14:21 -0400

    Post model unit testing updated

--- BackchannelApp/test/unit/post_test.rb
+++ BackchannelApp/test/unit/post_test.rb
@@ -5,4 +5,21 @@ class PostTest < ActiveSupport::TestCase
     p = Post.new
     assert !p.valid?
   end
+
+  test "should return non empty search with valid username" do
+    result = Post.search( 'username',  'member_1')
+    assert result.size > 0
+  end
+  test "should return non empty search with valid category" do
+    result = Post.search( 'category',  'Category1')
+    assert result.size > 0
+  end
+  test "should return empty search with invalid username" do
+    result = Post.search( 'username',  'member_x')
+    assert result.size == 0
+  end
+  test "should return empty search with invalid category" do
+    result = Post.search( 'category',  'CategoryX')
+    assert result.size == 0
+  end
 end
